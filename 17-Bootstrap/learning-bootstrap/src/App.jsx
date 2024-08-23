90f0d6f0a17f0f99b655d3592a30f5e87ff30ec8
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-08-23 13:47:49 +0530

    Incorporating Bootstrap To do app project

---
+++ 17-Bootstrap/learning-bootstrap/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h1>There should have been bootstrap here</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Projects/1-todo-app-v1/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <center className="todo-container">
+      <h1>TODO App</h1>
+    </center>
+  );
+}
+
+export default App;
